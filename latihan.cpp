4be45b3c10516aa0924fd1e72ece90c2a367cff5
MVRAKANDAW <mochammadvianto@gmail.com>
2017-02-09 21:38:19 +0700

    Delete latihan.cpp

--- latihan.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream> // library standar yang digunakan untuk input dan output
-using namespace std;
-
-int main() {
-   cout << "Hello World"; // menampilkan teks "Hello World"
-   return 0; // mengakhiri program
-}
